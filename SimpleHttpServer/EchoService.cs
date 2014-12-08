7c807fa60d568e13bda2924bf3a457bf01786a9f
romanoncha <romanoncha@mail.ru>
2014-12-08 11:06:22 +0200

    First Commit

---
+++ SimpleHttpServer/EchoService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    class EchoService
+    {
+        private TcpClient connectionSocket;
+        private String[] programs = {"Calculator", "Gallery", "MusicPlayer"};
+
+        public EchoService(TcpClient connectionSocket)
+        {
+            // TODO: Complete member initialization
+            this.connectionSocket = connectionSocket;
+        }
+        public void DoIt()
+        {
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer, command = "";
+            while (message != null && message != "")
+            {
+
+                //Console.WriteLine("Client: " + message);
+                answer = "Server started program ";
+                
+                if (message.IndexOf("GET /?") == 0)
+                {
+                    for (int i = 6; i < message.Length; i++)
+                    {
+                        if (message[i] != ' ')
+                            command += message[i];
+                        else
+                            break;
+                    }
+                    for (int i = 0; i < programs.Length; i++)
+                    {
+                        if (programs[i].ToLower().IndexOf(command) == 0)
+                            answer += programs[i];
+                    }
+                    Console.WriteLine("Command from client: " + command);
+                    if (answer == "Server started program ")
+                        sw.WriteLine("The program does not installed on server");
+                    else
+                        sw.WriteLine(answer);
+                }               
+
+                //sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+            connectionSocket.Close();
+        }
+
+        public string answer { get; set; }
+    }
+}
---
+++ SimpleHttpServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    internal class SimpleHttpServer
+    {
+        public static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 1616);
+
+            //TcpListener serverSocket = new TcpListener(1616);
+            serverSocket.Start();
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server activated now");
+                EchoService service = new EchoService(connectionSocket);
+                Thread myThread = new Thread(new ThreadStart(service.DoIt));
+                myThread.Start();
+            }
+            serverSocket.Stop();
+        }
+    }
+}
